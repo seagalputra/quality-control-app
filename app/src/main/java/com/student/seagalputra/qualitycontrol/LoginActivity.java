f1215580e9aa70b01161c3cb0cdd74b875f6ff8c
seagalputra <seagalputra@gmail.com>
2018-05-06 03:18:00 +0700

    Add bottom navigation bar

--- app/src/main/java/com/student/seagalputra/qualitycontrol/LoginActivity.java
+++ app/src/main/java/com/student/seagalputra/qualitycontrol/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.student.seagalputra.qualitycontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void mainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/student/seagalputra/qualitycontrol/MainActivity.java
@@ -0,0 +1,65 @@
+package com.student.seagalputra.qualitycontrol;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private HomeFragment homeFragment;
+    private CreateFragment createFragement;
+    private ProfileFragment profileFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        createFragement = new CreateFragment();
+        profileFragment = new ProfileFragment();
+
+        // Set default fragment
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_dashboard:
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.navigation_create:
+                        setFragment(createFragement);
+                        return true;
+
+                    case R.id.navigation_profile:
+                        setFragment(profileFragment);
+                        return true;
+
+                        default:
+                            return false;
+
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
