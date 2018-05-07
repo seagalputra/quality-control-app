1b3bc8b0be52cc73a157c559c27829247c0630f7
seagalputra <seagalputra@gmail.com>
2018-05-07 14:31:49 +0700

    Partially implement View Product

--- app/src/main/java/com/student/seagalputra/qualitycontrol/HomeFragment.java
+++ app/src/main/java/com/student/seagalputra/qualitycontrol/HomeFragment.java
@@ -3,6 +3,7 @@ package com.student.seagalputra.qualitycontrol;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
@@ -12,8 +13,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -24,9 +34,11 @@ public class HomeFragment extends Fragment {
 
     //Define variable for Product RecyclerView
     private List<Product> productList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private ProductAdapter pAdapter;
-    private DividerItemDecoration dividerItemDecoration;
+    //private RecyclerView recyclerView;
+    //private ProductAdapter pAdapter;
+    //private DividerItemDecoration dividerItemDecoration;
+    private ListView listView;
+    private String JSON_STRING;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -40,23 +52,58 @@ public class HomeFragment extends Fragment {
         View home_fragment = inflater.inflate(R.layout.fragment_home, container, false);
 
         // Create List for Product
-        recyclerView = (RecyclerView) home_fragment.findViewById(R.id.recyclerview_home);
-        pAdapter = new ProductAdapter(productList);
-        LinearLayoutManager pLayoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(pLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //recyclerView = (RecyclerView) home_fragment.findViewById(R.id.recyclerview_home);
+        //pAdapter = new ProductAdapter(productList);
+        //LinearLayoutManager pLayoutManager = new LinearLayoutManager(getActivity());
+        //recyclerView.setLayoutManager(pLayoutManager);
+        //recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         // Add divider in RecyclerView
-        dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), pLayoutManager.getOrientation());
-        recyclerView.addItemDecoration(dividerItemDecoration);
+        //dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), pLayoutManager.getOrientation());
+        //recyclerView.addItemDecoration(dividerItemDecoration);
 
         // Set RecyclerView adapter
-        recyclerView.setAdapter(pAdapter);
+        //recyclerView.setAdapter(pAdapter);
 
-        prepareProductData();
+        //prepareProductData();
+        //listView = (ListView) getContext().findViewById(R.id.listView);
+        listView = (ListView) getActivity().findViewById(R.id.listView);
+        // listView.setOnClickListener(this);
+        getJSON();
+
         return home_fragment;
     }
 
+    private void showProduct() {
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(Server.TAG_JSON_ARRAY);
+
+            for (int i = 0; i < result.length(); i++) {
+                JSONObject jo = result.getJSONObject(i);
+                String nameProduct = jo.getString(Server.TAG_JSON_PRODUCT_NAME);
+                String priceProduct = jo.getString(Server.TAG_JSON_PRODUCT_SELL_PRICE);
+
+                HashMap<String, String> product = new HashMap<>();
+                product.put(Server.TAG_JSON_PRODUCT_NAME,nameProduct);
+                product.put(Server.TAG_JSON_PRODUCT_SELL_PRICE, priceProduct);
+                list.add(product);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+                getContext(), list, R.layout.fragment_home, new String[]{Server.TAG_JSON_PRODUCT_ID, Server.TAG_JSON_PRODUCT_NAME}, new int[]{R.id.name_product, R.id.price_product});
+    }
+
+    private void getJSON() {
+
+    }
+
+    /*
     private void prepareProductData() {
         Product product = new Product("Swallow X", "Sepatu", "Rp 12.500");
         productList.add(product);
@@ -68,5 +115,5 @@ public class HomeFragment extends Fragment {
         productList.add(product);
 
         pAdapter.notifyDataSetChanged();
-    }
+    } **/
 }
--- app/src/main/java/com/student/seagalputra/qualitycontrol/Server.java
+++ app/src/main/java/com/student/seagalputra/qualitycontrol/Server.java
@@ -1,5 +1,33 @@
 package com.student.seagalputra.qualitycontrol;
 
 public class Server {
+    // Script PHP to get MySQL Data
     public static final String URL_LOGIN = "http://10.20.33.46/android_login_api/include/login.php";
+    public static final String URL_ADD_PRODUCT = "http://10.20.33.46/android_login_api/include/tambahProduct.php";
+    public static final String URL_GET_PRODUCT = "http://10.20.33.46/android_login_api/include/tampilProduct.php";
+    public static final String URL_GET_PRODUCT_ALL = "http://10.20.33.46/android_login_api/include/tampilSemuaProduct.php";
+
+    // Key to send request PHP script
+    public static final String KEY_PRODUCT_ID = "id_produk";
+    public static final String KEY_PRODUCT_NAME = "nama_produk";
+    public static final String KEY_PRODUCT_KIND = "jenis_produk";
+    public static final String KEY_PRODUCT_BRAND = "merk_produk";
+    public static final String KEY_PRODUCT_BUY_PRICE = "harga_beli";
+    public static final String KEY_PRODUCT_SELL_PRICE = "harga_jual";
+    public static final String KEY_PRODUCT_STATUS = "status_produk";
+    public static final String KEY_PRODUCT_TOTAL = "jml_produk";
+
+    // JSON Tags
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_JSON_PRODUCT_ID = "id_produk";
+    public static final String TAG_JSON_PRODUCT_NAME = "nama_produk";
+    public static final String TAG_JSON_PRODUCT_KIND = "jenis_produk";
+    public static final String TAG_JSON_PRODUCT_BRAND = "merk_produk";
+    public static final String TAG_JSON_PRODUCT_BUY_PRICE = "harga_beli";
+    public static final String TAG_JSON_PRODUCT_SELL_PRICE = "harga_jual";
+    public static final String TAG_JSON_PRODUCT_STATUS = "status_produk";
+    public static final String TAG_JSON_PRODUCT_TOTAL = "jml_produk";
+
+    // Product ID
+    public static final String PRODUCT_ID = "id";
 }
